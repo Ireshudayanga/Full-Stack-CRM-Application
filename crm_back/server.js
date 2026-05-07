0e0b6c74b5af874e68a258da461ba0893c15bfbc
Iresh Udayanga <123167032+Ireshudayanga@users.noreply.github.com>
2026-05-07 13:48:45 +0530

    Add request logger middleware for enhanced request tracking

--- crm_back/server.js
+++ crm_back/server.js
@@ -5,6 +5,7 @@ const connectDB = require('./config/db');
 
 const authRoutes = require('./routes/authRoutes');
 const leadRoutes = require('./routes/leadRoutes');
+const requestLogger = require('./middleware/loggerMiddleware');
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(requestLogger);
 
 // Database Connection
 connectDB();
